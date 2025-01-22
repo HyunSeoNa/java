6fd0ec82bea57e492b75fddfa972891609cbb2e3
nahs <nahs3467@gmail.com>
2025-01-22 11:41:02 +0900

    [2025.01.22 nahs] 백준 32978 추가

---
+++ src/main/java/com/nahs/BaekJoon32978.java
@@ -0,0 +1,35 @@
+package com.nahs;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashSet;
+
+/**
+ * fileName     : BaekJoon32978
+ * author       : HyunSeo Na
+ * date         : 2025-01-22
+ * description  : 백준 32978 아 맞다 마늘
+ */
+public class BaekJoon32978 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int count = Integer.parseInt(br.readLine());
+
+        String line = br.readLine();
+        String[] selection = line.split(" ");
+        Arrays.sort(selection);
+
+        line = br.readLine();
+        String[] used = line.split(" ");
+        HashSet<String> hashSet = new HashSet<>(Arrays.asList(selection));
+
+        for(String s : used) {
+            hashSet.remove(s);
+        }
+
+        System.out.println(hashSet.iterator().next());
+    }
+}
